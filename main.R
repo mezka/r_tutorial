1c53f7b47300ed710b0f2a5f9abe311732474296
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2022-03-24 04:08:03 -0300

    Ejercicios del 1 al 5

---
+++ main.R
@@ -0,0 +1,33 @@
+# 1
+
+primerObjeto <- 6 * 4
+
+print(primerObjeto)
+
+# 2
+
+ingresos <- c(45000, 70000, 90000, 25000)
+
+print(ingresos)
+
+# 3
+
+ingresos[3] <- 123
+
+print(ingresos)
+
+# 4
+
+ingresosComoTexto <- c("CUARENTAICINCOMIL", "SETENTAMIL", "NOVENTAMIL", "VEINTICINCOMIL")
+
+print(ingresosComoTexto)
+
+#5
+
+valores <- c("HASTA PRIMARIO COMPLETO", "HASTA SECUNDARIO COMPLETO", "HASTA TERCIARIO COMPLETO")
+
+nivel_educativo <- factor(valores)
+
+print(nivel_educativo)
+print(nlevels(nivel_educativo))
+
