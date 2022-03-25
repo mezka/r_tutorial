5d7d00c36e9dd005de70b902e816cfa138df4fba
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2022-03-24 21:21:01 -0300

    Ejericios del 5 al 7

--- main.R
+++ main.R
@@ -24,10 +24,24 @@ print(ingresosComoTexto)
 
 #5
 
-valores <- c("HASTA PRIMARIO COMPLETO", "HASTA SECUNDARIO COMPLETO", "HASTA TERCIARIO COMPLETO")
+valores <- c("HASTA PRIMARIO COMPLETO", "HASTA SECUNDARIO COMPLETO", "HASTA TERCIARIO COMPLETO", "HASTA UNIVERSITARIO COMPLETO")
 
 nivel_educativo <- factor(valores)
 
 print(nivel_educativo)
 print(nlevels(nivel_educativo))
 
+#6
+
+miDataFrame <- data.frame(nivel_educativo = nivel_educativo, ingresos = ingresos)
+
+print(miDataFrame)
+
+#7
+
+dim(miDataFrame)
+summary(miDataFrame)
+
+#8
+
+print(mean(ingresos))
